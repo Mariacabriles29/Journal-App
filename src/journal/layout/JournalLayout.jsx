bfa81620e6f2aadc3318df698627cb1734854399
mariacabriles29 <mecp2992@gmail.com>
2023-06-09 09:05:19 -0400

    feat: create register page

---
+++ src/journal/layout/JournalLayout.jsx
@@ -0,0 +1,12 @@
+import { Box } from "@mui/system";
+export const JournalLayout = () => {
+  return (
+    <Box sx={{display:'flex'}}>
+        {/* Navbar */}
+
+        
+        {/* sidebar */}
+
+    </Box>
+  )
+}
