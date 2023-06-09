40204032c3b0faf269657d36d9530a13f2e56b7f
mariacabriles29 <mecp2992@gmail.com>
2023-06-09 12:56:13 -0400

    feat: add note view

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,16 +1,21 @@
 
-import { Typography } from "@mui/material"
 import { JournalLayout } from "../layout/JournalLayout"
+import { NoteView } from "../views/NoteView"
+// import { NothingSelectedView } from "../views/NothingSelectedView"
 
 export const JournalPage = () => {
   
   return (
     <>
     <JournalLayout> 
-    <Typography>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero, nesciunt. Assumenda tenetur itaque aliquam doloremque vero vitae impedit, ratione quisquam inventore, aperiam voluptates minus illum laboriosam eaque? Mollitia, eos sapiente!</Typography>
-    
+    {/* <Typography>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero, nesciunt. Assumenda tenetur itaque aliquam doloremque vero vitae impedit, ratione quisquam inventore, aperiam voluptates minus illum laboriosam eaque? Mollitia, eos sapiente!</Typography>
+     */}
     {/* NothinSelected */}
+   {/* cuando no hay una nota selecionada exporto los colores del view */}
+    {/* <NothingSelectedView/> */}
+
     {/* NoteView */}
+    <NoteView/>
     </JournalLayout>
     </>
       )
