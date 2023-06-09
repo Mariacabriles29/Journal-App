ceea245e503fe48821542a3ef1c60c595410321b
mariacabriles29 <mecp2992@gmail.com>
2023-06-08 22:00:05 -0400

    feat: finishing login without layout

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,50 +1,64 @@
-import { Grid, TextField, Typography } from "@mui/material"
+import { Google } from "@mui/icons-material";
+import { Button, Grid, TextField, Typography,Link } from "@mui/material";
+import { Link as RouterLink } from "react-router-dom";
 export const LoginPage = () => {
   return (
-  <Grid 
-  container
-  spacing={ 0 }
-  direction="column"
-  alignItems="center"
-  justifyContent="center"
-  /* Para establecer tama#o y color  es moado porque en el archivo themePurple lo defini asi */
-  sx={{minHeight:'100vh', backgroundColor:'primary.main', padding:4}}
+    <Grid
+      container
+      spacing={0}
+      direction="column"
+      alignItems="center"
+      justifyContent="center"
+      /* Para establecer tama#o y color  es moado porque en el archivo themePurple lo defini asi */
+      sx={{ minHeight: "100vh", backgroundColor: "primary.main", padding: 4 }}
+    >
+      <Grid
+        item
+        className="box-shadow"
+        /*xs par el tama#p de las pantalla  */
+        xs={3}
+        sx={{ backgroundColor: "white", padding: 3, borderRadius: 2 }}
+      >
+        <Typography variant="h5" sx={{ mb: 1 }}>
+          Login
+        </Typography>
+        <form>
+          <Grid container>
+            <Grid item xs={12} sx={{ mt: 2 }}>
+              <TextField
+                label="correo"
+                type="email"
+                placeholder="correo@google.com"
+                fullWidth
+              />
+            </Grid>
+            <Grid item xs={12} sx={{ mt: 2 }}>
+              <TextField
+                label="Contraseña"
+                type="password"
+                placeholder="Contraseña"
+                fullWidth
+              />
+            </Grid>
+            <Grid container spacing={2} sx={{ mb: 2, mt: 2 }}>
+              <Grid item xs={12} sm ={6}>
+                <Button variant="contained" fullWidth >Login</Button>
+              </Grid>
+              <Grid item xs={12} sm ={6}>
+                <Button variant="contained" fullWidth >
+                  <Google/>
+                  <Typography sx={{ml:1}}>Google</Typography>
+                </Button>
+              </Grid>
+            </Grid>
+            <Grid container direction='row' justifyContent='end'>
+              <Link component={RouterLink} color='inherit' to="/auth/register">Crear una cuenta</Link>
+                
 
-
-  >
-    <Grid item
-    className='box-shadow'
-    /*xs par el tama#p de las pantalla  */
-    xs={ 3 }
-    sx={{ backgroundColor:'white', padding:3,borderRadius:2}}>
-      <Typography variant="h5" sx={{mb:1}}>Login</Typography> 
-      <form>
-        <Grid container>
-          <Grid item xs={12} sx={{mt:2}}>
-            <TextField 
-            label="correo"
-            type="email"
-            placeholder="correo@google.com"
-            fullWidth
-            
-            />
+            </Grid>
           </Grid>
-           <Grid item xs={12} sx={{mt:2}}>
-            <TextField 
-            label="Contraseña"
-            type="password"
-            placeholder="Contraseña"
-            fullWidth
-            
-            />
-          </Grid>
-        </Grid>
-      </form>
-    
-
+        </form>
+      </Grid>
     </Grid>
-
-  </Grid>
-    
-  )
-}
+  );
+};
