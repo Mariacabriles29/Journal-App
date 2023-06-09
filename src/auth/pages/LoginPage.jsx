fb4d4814acfe3fad4c8a3955c7b93253f3e3393d
mariacabriles29 <mecp2992@gmail.com>
2023-06-08 21:30:21 -0400

    feat: create login

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -20,11 +20,20 @@ export const LoginPage = () => {
       <Typography variant="h5" sx={{mb:1}}>Login</Typography> 
       <form>
         <Grid container>
-          <Grid item>
+          <Grid item xs={12} sx={{mt:2}}>
             <TextField 
             label="correo"
             type="email"
             placeholder="correo@google.com"
+            fullWidth
+            
+            />
+          </Grid>
+           <Grid item xs={12} sx={{mt:2}}>
+            <TextField 
+            label="Contraseña"
+            type="password"
+            placeholder="Contraseña"
             fullWidth
             
             />
